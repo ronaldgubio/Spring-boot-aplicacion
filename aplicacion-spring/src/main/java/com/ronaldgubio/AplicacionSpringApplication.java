d12cde9dde2f679f34d0bca0950169f989f64a1b
adminltg <adminltg@sisuser01.latablitagroup.com>
2020-03-26 19:08:57 -0500

    commit inicial con eclipse

---
+++ aplicacion-spring/src/main/java/com/ronaldgubio/AplicacionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ronaldgubio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionSpringApplication.class, args);
+	}
+
+}
